ffac54787adeb9785ff23bb4bd9fba4b17e708bc
jpd5367 <lilgsrsoldier@gmail.com>
2014-09-30 16:52:09 -0500

    Initial Commit

---
+++ Parkway/Assets/Scripts/GameManager.cs
@@ -0,0 +1,18 @@
+﻿// This script will manage global variables and activities
+
+
+using UnityEngine;
+using System.Collections;
+
+public class GameManager : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
